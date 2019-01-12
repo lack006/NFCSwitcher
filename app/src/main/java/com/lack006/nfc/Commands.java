2cb95b8b8ce453d6d30ea62867829125f8a236df
lack006 <lack00006@gmail.com>
2019-01-12 23:27:49 +0800

    code fix

--- app/src/main/java/com/lack006/nfc/Commands.java
+++ app/src/main/java/com/lack006/nfc/Commands.java
@@ -1,6 +1,6 @@
 package com.lack006.nfc;
 
-public class Commands {
-    public static final String NFC_ON_SHELL = "svc nfc enable";
-    public static final String NFC_OFF_SHELL = "svc nfc disable";
+class Commands {
+    static final String NFC_ON_SHELL = "svc nfc enable";
+    static final String NFC_OFF_SHELL = "svc nfc disable";
 }
